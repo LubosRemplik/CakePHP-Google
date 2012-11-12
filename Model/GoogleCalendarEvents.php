142aacd20fedcd299d674d7bc2aa2fb233d61f16
Lubos Remplik <lubos@on-idle.com>
2012-11-12 12:25:46 +0000

    Delete, deleteAll and update method in GoogleCalendarEvents model added

--- Model/GoogleCalendarEvents.php
+++ Model/GoogleCalendarEvents.php
@@ -12,6 +12,43 @@ class GoogleCalendarEvents extends GoogleApi {
 	);
 
 	/**
+	 * https://developers.google.com/drive/v2/reference/files/delete
+	 **/
+	public function delete($calendarId, $eventId, $options = array()) {
+		$path = sprintf('/%s/events/%s', $calendarId, $eventId);
+		$request = array();
+		$request['method'] = 'DELETE';
+		if ($options) {
+			$request['uri']['query'] = $options;
+		}
+		return $this->_request($path, $request);
+	}
+
+	/**
+	 * list items between $from and $to and then delete them
+	 **/
+	public function deleteAll($from, $to, $calendarId, $options = array()) {
+		$deletedAll = true;
+		$listOptions = array(
+			'timeMin' => date('c', strtotime($from)),
+			'timeMax' => date('c', strtotime($to))
+		);
+		Cache::clear();
+		$events = $this->listItems($calendarId, $listOptions);
+		while (count($events['items']) > 0) {
+			foreach ($events['items'] as $item) {
+				$deleted = $this->delete($calendarId, $item['id']);
+				if (!empty($deleted)) {
+					$deletedAll = false;
+				}
+			}
+			Cache::clear();
+			$events = $this->listItems($calendarId, $listOptions);
+		}
+		return $deletedAll;
+	}
+
+	/**
 	 * https://developers.google.com/google-apps/calendar/v3/reference/events/insert
 	 *
 	 * $calendarId the calendar you want to insert event
@@ -38,5 +75,22 @@ class GoogleCalendarEvents extends GoogleApi {
 			$request['uri']['query'] = $options;
 		}
 		return $this->_request(sprintf('/%s/events', $calendarId), $request);
+	}
+
+	/**
+	 * https://developers.google.com/google-apps/calendar/v3/reference/events/update
+	 *
+	 * Similar to insert method
+	 **/
+	public function update($calendarId, $eventId, $data, $options = array()) {
+		$path = sprintf('/%s/events/%s', $calendarId, $eventId);
+		$request = array();
+		$request['method'] = 'PUT';
+		if ($options) {
+			$request['uri']['query'] = $options;
+		}
+		$request['body'] = json_encode($data);
+		$request['header']['Content-Type'] = 'application/json';
+		return $this->_request($path, $request);
 	}
 }
